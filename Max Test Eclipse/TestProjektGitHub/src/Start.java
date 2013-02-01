7fd49d6787b82b2f3e2ad4f3ce178644d91bdd53
maxfremars <max.kuhlmay@icloud.com>
2013-02-01 08:06:02 +0100

    Max

--- Max Test Eclipse/TestProjektGitHub/src/Start.java
+++ Max Test Eclipse/TestProjektGitHub/src/Start.java
@@ -6,9 +6,13 @@ public class Start {
 	public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
+<<<<<<< HEAD
 		System.out.printf("Irgendwas stimmt nicht so wirklich");
+=======
+		System.out.printf("Irgendwas stimmt nicht");
+>>>>>>> 276c1290331baa8e6ab9e845c1c67378cdec0488
 
-	System.out.println("Ich bin jetzt in meinem Branch");
+		System.out.println("Ich bin jetzt in meinem Branch");
 
 	
 	}
