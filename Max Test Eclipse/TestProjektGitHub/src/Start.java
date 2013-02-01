11e6f7593a4121278e5926ef4676c6bea91bc7a3
maxfremars <max.kuhlmay@icloud.com>
2013-02-01 07:51:12 +0100

    Max

--- Max Test Eclipse/TestProjektGitHub/src/Start.java
+++ Max Test Eclipse/TestProjektGitHub/src/Start.java
@@ -7,20 +7,15 @@ public class Start {
 		
 		System.out.println("Hallo Welt,   jfle Max");
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-=======
 		System.out.printf("Irgendwas stimmt nicht so wirklich");
->>>>>>> Max
 
 	System.out.println("Hallo Welt! TEST!");
 	System.out.println("TESTTEST");
 	
-<<<<<<< HEAD
 		System.out.printf("Irgendwas stimmt nicht");
 		System.out.printf("Irgendwas stimmt nicht so wirklich");
 
-=======
->>>>>>> Max
+
 	}
 
 }
