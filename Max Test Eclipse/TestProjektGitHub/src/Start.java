3b81e0c8e25a7d405335b6226d20d5d2b9998bf6
maxfremars <max.kuhlmay@icloud.com>
2013-02-01 08:06:51 +0100

    Max

--- Max Test Eclipse/TestProjektGitHub/src/Start.java
+++ Max Test Eclipse/TestProjektGitHub/src/Start.java
@@ -9,8 +9,8 @@ public class Start {
 <<<<<<< HEAD
 		System.out.printf("Irgendwas stimmt nicht so wirklich");
 =======
-		System.out.printf("Irgendwas stimmt nicht");
 >>>>>>> 276c1290331baa8e6ab9e845c1c67378cdec0488
+		System.out.printf("Irgendwas stimmt nicht");
 
 		System.out.println("Ich bin jetzt in meinem Branch");
 
